bb0d6892b30a1a0db85051639d293714517465f2
zayntkj24 <ndazzz2408@gmail.com>
2026-09-10 11:01:51 +0700

    Delete supabase directory

--- supabase/migrations/20260521145743_0feb6731-41be-4ec2-a4d8-2f2a41426518.sql
+++
@@ -1,4 +0,0 @@
-
-alter function public.touch_updated_at() set search_path = public;
-
-revoke execute on function public.has_role(uuid, public.app_role) from anon, authenticated, public;
--- supabase/migrations/20260521145803_f2e4f612-87c3-41aa-8872-d3f1ac1672ca.sql
+++
@@ -1,1 +0,0 @@
-drop policy if exists "public read doc-images" on storage.objects;
