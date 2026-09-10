9fa64c10426f59b45d32d42aa555bba3c7549d7f
zayntkj24 <ndazzz2408@gmail.com>
2026-09-10 10:59:42 +0700

    Delete src directory

--- src/docs/linux/wazuh.md
+++
@@ -1,4 +0,0 @@
-# Installasi Wazuh
-
-> Installasi wazuh server di Ubuntu server 24.04 dan Wazuh agent di Debian 12
-
--- src/routes/portfolio.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { PortfolioPage } from "@/portfolio";
-
-export const Route = createFileRoute("/portfolio")({
-  component: PortfolioPage,
-});
